2c0961188dc8dd08bdbb9a1bdcaf8c29b2f9d00b
Wojciech Tobiś <wojciech.tobis@relativity.com>
2022-05-14 18:43:31 +0200

    Add refresh operation

--- pulumi-basic/Invoke-PulumiLocal.ps1
+++ pulumi-basic/Invoke-PulumiLocal.ps1
@@ -3,8 +3,14 @@ try {
     Write-Host "Pulumi login"
     pulumi login file://$localPulumiPath
 
+    Write-Host "Pulumi refresh"
+    pulumi refresh
+
     Write-Host "Pulumi up"
     pulumi up
+
+    # Write-Host "Pulumi refresh + up"
+    # pulumi up --refresh
 }
 finally {
     pulumi logout
