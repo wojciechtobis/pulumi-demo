78a402e107f638f6fb4b604263cbb558774a52d5
Wojciech Tobiś <wojciech.tobis@relativity.com>
2022-05-15 01:28:05 +0200

    Change operations order

--- pulumi-basic/Invoke-PulumiLocal.ps1
+++ pulumi-basic/Invoke-PulumiLocal.ps1
@@ -6,8 +6,11 @@ try {
     Write-Host "Pulumi up"
     pulumi up
 
-    # Write-Host "Pulumi import"
-    # pulumi import azure-native:storage:BlobContainer test4 /subscriptions/$subscriptionId/resourceGroups/Pulumi-basic-local-demo/providers/Microsoft.Storage/storageAccounts/pulumisae037acfb/blobServices/default/containers/test4
+    Write-Host "Pulumi import"
+    pulumi import azure-native:storage:BlobContainer test4 /subscriptions/$subscriptionId/resourceGroups/Pulumi-basic-local-demo/providers/Microsoft.Storage/storageAccounts/pulumisae037acfb/blobServices/default/containers/test4
+
+    Write-Host "Pulumi up"
+    pulumi up
 }
 finally {
     pulumi logout
