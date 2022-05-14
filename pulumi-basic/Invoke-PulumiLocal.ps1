f8befc84040c6ded495e4370a2d01097a942f208
Wojciech Tobiś <wojciech.tobis@relativity.com>
2022-05-15 01:22:50 +0200

    Change operations order

--- pulumi-basic/Invoke-PulumiLocal.ps1
+++ pulumi-basic/Invoke-PulumiLocal.ps1
@@ -3,11 +3,11 @@ try {
     Write-Host "Pulumi login"
     pulumi login file://$localPulumiPath
 
+    Write-Host "Pulumi state delete"
+    pulumi state delete urn:pulumi:local::azure-csharp::azure-native:storage:BlobContainer::test4
+
     Write-Host "Pulumi up"
     pulumi up
-
-    # Write-Host "Pulumi state delete"
-    # pulumi state delete urn:pulumi:local::azure-csharp::azure-native:storage:BlobContainer::test4
 }
 finally {
     pulumi logout
